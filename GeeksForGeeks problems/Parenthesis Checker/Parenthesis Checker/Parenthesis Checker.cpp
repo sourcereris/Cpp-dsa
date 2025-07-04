287269234812b386d230b14508feecb15e9237cf
Rimvydas <rimedimas@gmail.com>
2025-07-04 11:40:04 +0900

    final version, (using stack)

--- GeeksForGeeks problems/Parenthesis Checker/Parenthesis Checker/Parenthesis Checker.cpp
+++ GeeksForGeeks problems/Parenthesis Checker/Parenthesis Checker/Parenthesis Checker.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
 #include <string>
-
+#include <stack>
 using namespace std;
 
-bool solution(string);
+bool solution(string&);
 
 int main()
 {
-    string s = "[(({[])})]";
+    string s = "[({}((){([])})())]";
     
-    s.erase(0, 1);
+    //s.erase(0, 1);
     cout << s << '\n';
     cout << solution(s);
 }
 
-bool solution(string s) {
+bool solution(string& s) {
     int size = s.size();
     if (size % 2 != 0) return false;
-    
-    return true;
+    stack<char> sc;
+
+    for (char ch : s) {
+
+        if (ch == '(' || ch == '[' || ch == '{') sc.push(ch);
+        else {
+            if (sc.empty()) return false;
+            char top = sc.top();
+            if (ch == ']' && top == '[' 
+             || ch == ')' && top == '('
+             || ch == '}' && top == '{')
+                sc.pop();
+            else return false;
+        }
+    }
+    return sc.empty();
 }
